53007715400792f570a6988f5bb23a0a05e63c0d
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-08 15:51:46 +0100

    added test and fixing issues with common library

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -4,7 +4,11 @@ import { app } from '../../app';
 const http = supertest(app);
 
 it('has a route handler listening to /api/tickets/ for post requests', async () => {
-  // const response = await(request)
+  const response = await http
+    .post('/api/tickets')
+    .send({});
+  
+  expect(response.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
