d7b4114d97e80e49bea040e06b1d3f838313a1db
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-14 07:16:33 +0100

    finished show ticket test

--- src/routes/__test__/show.test.ts
+++ src/routes/__test__/show.test.ts
@@ -1,13 +1,17 @@
 import supertest from 'supertest';
+import mongoose from 'mongoose';
 import { app } from '../../app';
 
 const http = supertest(app);
 
 it ('returns a 404 if the ticket is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
   await http
-    .get('/api/tickets/lasjweoiwefakjpo')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
+
 });
 
 it ('returns the ticket if the ticket is found', async () => {
--- src/routes/show.ts
+++ src/routes/show.ts
@@ -1,10 +1,17 @@
+import { BadRequestError, NotFoundError } from '@mfticketing/common';
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.get('/api/tickets/:id', async (req:Request, res:Response) => {
+  const ticket = await Ticket.findById(req.params.id);
 
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.json(ticket);
 });
 
 export { router as showTicketRouter };
