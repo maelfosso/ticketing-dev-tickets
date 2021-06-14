4a61b5238ae5001379a63ff18d40741c3a810d20
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-14 07:48:00 +0100

    added test to handle wront title,price for updating

--- src/routes/__test__/update.test.ts
+++ src/routes/__test__/update.test.ts
@@ -36,7 +36,33 @@ it ('returns a 401 if user does not own the ticket', async () => {
 });
 
 it ('returns 400 if the user provides an invalid title or price', async () => {
+  const cookie = global.signin();
 
+  const response = await http
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'aslkdw',
+      price: 20
+    });
+
+  await http
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20
+    })
+    .expect(400);
+
+  await http
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'ijepourwa',
+      price: -20
+    })
+    .expect(400);
 });
 
 it ('updates the ticket provided valid inputs', async () => {
--- src/routes/update.ts
+++ src/routes/update.ts
@@ -13,6 +13,15 @@ const router = express.Router();
 router.put(
   '/api/tickets/:id',
   requireAuth,
+  [
+    body('title')
+      .not()
+      .isEmpty()
+      .withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and greater than 0')
+  ],
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
