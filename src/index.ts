c9274a7215b96f5d5be5927a1c2f49385540907b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-26 07:48:40 +0100

    added a nats wrapper - singleton

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import { randomBytes } from 'crypto';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.MONGO_URI) {
@@ -11,6 +13,14 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', randomBytes(4).toString('hex'), 'https://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    })
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- src/routes/new.ts
+++ src/routes/new.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@mfticketing/common';
 import { Ticket } from '../models/ticket';
-import { TicketCreatedPublisher } from 'src/events/publishers/ticket-created-publisher';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -31,7 +32,7 @@ router.post(
 
   await ticket.save();
 
-  new TicketCreatedPublisher(client).publish({
+  await (new TicketCreatedPublisher(natsWrapper.client)).publish({
     id: ticket.id,
     title: ticket.title,
     price: ticket.price,
